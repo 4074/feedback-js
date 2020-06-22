26b684504aabfef96b8cf8087107b9bc3ed3bb2d
小峰 <dwfn3398@corp.netease.com>
2020-06-22 15:38:01 +0800

    Add input accept image and video

--- src/renders/modal.ts
+++ src/renders/modal.ts
@@ -59,7 +59,7 @@ export default class Modal {
             </div>
             <div class="feedback-form-item">
               <div class="feedback-form-label">${strings.labels?.image}</div>
-              <input type="file" class="feedback-input-file" multiple />
+              <input type="file" class="feedback-input-file" multiple accept="image/jpg,image/jpeg,image/png,image/gif,video/mp4" />
               <div class="feedback-form-uploader">
                 <div class="feedback-form-image feedback-uploader-trigger">
                   ${iconImage}
